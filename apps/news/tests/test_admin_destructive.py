c7c51137cfe6c96f3a775df4516b9df5a0a096c9
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-23 13:31:34 +0000

    Update test_admin_destructive.py

--- apps/news/tests/test_admin_destructive.py
+++ apps/news/tests/test_admin_destructive.py
@@ -10,16 +10,16 @@ import mock
 class TestArticleAdminBase(TestCase):
 
     def test_article_admin(self):
-        module = sys.modules['{{cookiecutter.repo_name}}.apps.news.admin']
-        del sys.modules['{{cookiecutter.repo_name}}.apps.news.admin']
+        module = sys.modules['{{ project_name }}.apps.news.admin']
+        del sys.modules['{{ project_name }}.apps.news.admin']
 
         admin.site.unregister(Article)
         admin.site.unregister(Category)
 
-        with mock.patch('{{cookiecutter.repo_name}}.apps.news.admin.externals.reversion', new=False):
+        with mock.patch('{{ project_name }}.apps.news.admin.externals.reversion', new=False):
             from ..admin import ArticleAdmin, ArticleAdminBase
             assert ArticleAdmin
             self.assertIs(ArticleAdmin.__bases__[0], ArticleAdminBase)
             self.assertEqual(len(ArticleAdmin.__bases__), 1)
 
-        sys.modules['{{cookiecutter.repo_name}}.apps.news.admin'] = module
+        sys.modules['{{ project_name }}.apps.news.admin'] = module
